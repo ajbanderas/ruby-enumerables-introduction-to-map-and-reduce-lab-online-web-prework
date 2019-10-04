b79ea85cadbf145d7c5581dd0a1604530a48f1d3
Kevin Downs <kevin.downs04@gmail.com>
2019-10-04 02:17:59 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -47,7 +47,15 @@ def reduce_to_total(source_array, starting_point = 0)
 end
 
 def reduce_to_all_true(source_array)
-  
+  counter = 0
+  while counter < source_array.length do
+    if source_array[counter]
+      counter += 1
+    else
+      return false
+    end
+  end
+  return true
 end
 
 
