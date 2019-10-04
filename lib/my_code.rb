1ed58741a8db505a8d6b39f6ad631dcce2c08357
Kevin Downs <kevin.downs04@gmail.com>
2019-10-04 02:05:18 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -37,4 +37,11 @@ end
 #reduce-like methods
 
 def reduce_to_total(source_array, strating_point = 0)
+  total = 0
+  counter = starting_point
+  while counter < source_array.length do
+    total += source_array[counter]
+    counter += 1
+  end
+  return total
 end
